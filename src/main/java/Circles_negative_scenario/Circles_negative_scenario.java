daa97fed88f1dad1f917febd7b6604901c2806fe
Sariga Sajeev <53913474+SarigaMallika@users.noreply.github.com>
2021-06-28 08:46:17 +0530

    Update Circles_negative_scenario.java

--- src/main/java/Circles_negative_scenario/Circles_negative_scenario.java
+++ src/main/java/Circles_negative_scenario/Circles_negative_scenario.java
@@ -39,23 +39,18 @@ public class Circles_negative_scenario extends Generic_functions{
 	@When("Click drop down list and do not make a selection and proceed then user should not be able to proceed to next page")
 	public void circle_positive_tc_001() throws Exception {
 		try {		
-//			click("home_circles");
-//			page_wait(12);
-//			value = driver.findElement(By.xpath(OR_reader("Object Locator", "circles_title"))).isDisplayed();
-//			Assert.assertEquals(true,value);	
-//			System.out.println("1.1");
-//			click("circles_add_partner_button");
-//			System.out.println("1.2");
-//			page_wait(20);
-//			click("circles_add_partner_dropdown");
-//			click("circles_add_partner_dropdown");
-//			System.out.println("1.3");
-//			page_wait(30);
-//			click("circles_proceed_button");
-//			System.out.println("1.4");
-//			page_wait(20);
-//			Assert.assertEquals(driver.findElement(By.xpath(OR_reader("Object Locator","circles_proceed_button"))).isEnabled(),true);
-			System.out.println("1");
+			click("home_circles");
+			page_wait(12);
+			value = driver.findElement(By.xpath(OR_reader("Object Locator", "circles_title"))).isDisplayed();
+			Assert.assertEquals(true,value);	
+			click("circles_add_partner_button");
+			page_wait(20);
+			click("circles_add_partner_dropdown");
+			click("circles_add_partner_dropdown");
+			page_wait(30);
+			click("circles_proceed_button");
+			page_wait(20);
+			Assert.assertEquals(driver.findElement(By.xpath(OR_reader("Object Locator","circles_proceed_button"))).isEnabled(),true);
 		} catch (Exception e) {
 			e.printStackTrace();
 			takeScreenShot("circle_negative_tc_001");
@@ -75,134 +70,112 @@ public class Circles_negative_scenario extends Generic_functions{
 			click("circles_add_partner_dropdown");
 			click("circles_mymedicare_option");
 			click("circles_mymedicare_option");
-			System.out.println("2.1");
 			page_wait(20);
 			click("circles_proceed_button_after_selection");
 			click("circles_proceed_button_after_selection");
-			System.out.println("2.1.1");
-			Thread.sleep(5000);
-			System.out.println("2.2");
 			Thread.sleep(5000);
 			click("circles_mymedicare_login_button");
 			click("circles_mymedicare_login_button");
-			System.out.println("2.3");
 			page_wait(40);
 			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username_valid_msg"))).getText();
-			System.out.println(str);
 			Assert.assertEquals(str,td_reader("circles_mymedicare_username_valid_msg"));
-			System.out.println("2.4");
 			page_wait(40);
 			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password_valid_msg"))).getText();
-			System.out.println(str);
 			Assert.assertEquals(str,td_reader("circles_mymedicare_password_valid_msg"));
-			System.out.println("2.5");
 		}catch (Exception e) {
 			e.printStackTrace();
 			takeScreenShot("circle_negative_tc_002");
 		}	
 	}
 
-//	/*TC 003 - Validate that the user should get a validation message on entering invalid Username  and Password in the Login Page*/
-//	@Given("Enter incorrect data in the  Username and Password field and verify that the user is getting the validation message")
-//	public void circle_positive_tc_003() throws Exception {
-//		try {
-//			System.out.println("3.1");
-//			click("circles_mymedicare_username");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",1));
-//			System.out.println("3.2");
-//			click("circles_mymedicare_password");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
-//			System.out.println("3.3");
-//			click("circles_mymedicare_login_button");
-//			field_clear("circles_mymedicare_username");
-//			field_clear("circles_mymedicare_username");
-//			System.out.println("3.4");
-//			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
-//			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
-//			System.out.println("3.5");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			takeScreenShot("circle_negative_tc_003");
-//		}	
-//	}
-//
-//	/*TC 004 - Validate that the user is not allowed leaves blank Username field*/
-//	@Given("User will get validation message when Username field is blank")
-//	public void circle_positive_tc_004() throws Exception {
-//		try {
-//			System.out.println("4.1");
-//			click("circles_mymedicare_password");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",0));
-//			System.out.println("4.2");
-//			click("circles_mymedicare_login_button");
-//			System.out.println("4.3");
-//			field_clear("circles_mymedicare_username");
-//			field_clear("circles_mymedicare_username");
-//			System.out.println("4.4");
-//			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
-//			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
-//			System.out.println("4.5");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			takeScreenShot("circle_negative_tc_004");
-//		}	
-//	}
-//
-//	/*TC 005 - Validate that the user is not allowed leaves blank Password field*/
-//	@Given("User will get validation message when Password field is blank")
-//	public void circle_positive_tc_005() throws Exception {
-//		try {
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
-//			click("circles_mymedicare_login_button");
-//			field_clear("circles_mymedicare_username");
-//			field_clear("circles_mymedicare_username");
-//			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
-//			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
-//			System.out.println("5");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			takeScreenShot("circle_negative_tc_005");
-//		}	
-//	}
-//
-//	/*TC 006 - Validate that the user is not allowed to login with valid Usename and invalid Password*/
-//	@Given("User will get a validation message when login with valid Usename and invalid Password")
-//	public void circle_positive_tc_006() throws Exception {
-//		try {
-//			click("circles_mymedicare_username");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
-//			click("circles_mymedicare_password");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
-//			click("circles_mymedicare_login_button");
-//			field_clear("circles_mymedicare_username");
-//			field_clear("circles_mymedicare_username");
-//			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
-//			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
-//			System.out.println("6");
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			takeScreenShot("circle_negative_tc_006");
-//		}	
-//	}
-//
-//	/*TC 007 - Validate that the user is not allowed to login with invalid Usename and valid Password */
-//	@Given("User will get a validation message when login with invalid Usename and valid Password")
-//	public void circle_positive_tc_007() throws Exception {
-//		try {
-//			click("circles_mymedicare_username");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
-//			click("circles_mymedicare_password");
-//			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
-//			click("circles_mymedicare_login_button");
-//			field_clear("circles_mymedicare_username");
-//			field_clear("circles_mymedicare_username");
-//			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
-//			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
-//			System.out.println("7");
-//			close();
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//			takeScreenShot("circle_negative_tc_007");
-//		}	
-//	}
+	/*TC 003 - Validate that the user should get a validation message on entering invalid Username  and Password in the Login Page*/
+	@Given("Enter incorrect data in the  Username and Password field and verify that the user is getting the validation message")
+	public void circle_positive_tc_003() throws Exception {
+		try {
+			click("circles_mymedicare_username");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",1));
+			click("circles_mymedicare_password");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
+			click("circles_mymedicare_login_button");
+			field_clear("circles_mymedicare_username");
+			field_clear("circles_mymedicare_username");
+			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
+			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
+		}catch (Exception e) {
+			e.printStackTrace();
+			takeScreenShot("circle_negative_tc_003");
+		}	
+	}
+
+	/*TC 004 - Validate that the user is not allowed leaves blank Username field*/
+	@Given("User will get validation message when Username field is blank")
+	public void circle_positive_tc_004() throws Exception {
+		try {
+			click("circles_mymedicare_password");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",0));
+			click("circles_mymedicare_login_button");
+			field_clear("circles_mymedicare_username");
+			field_clear("circles_mymedicare_username");
+			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
+			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
+		}catch (Exception e) {
+			e.printStackTrace();
+			takeScreenShot("circle_negative_tc_004");
+		}	
+	}
+
+	/*TC 005 - Validate that the user is not allowed leaves blank Password field*/
+	@Given("User will get validation message when Password field is blank")
+	public void circle_positive_tc_005() throws Exception {
+		try {
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
+			click("circles_mymedicare_login_button");
+			field_clear("circles_mymedicare_username");
+			field_clear("circles_mymedicare_username");
+			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
+			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
+		}catch (Exception e) {
+			e.printStackTrace();
+			takeScreenShot("circle_negative_tc_005");
+		}	
+	}
+
+	/*TC 006 - Validate that the user is not allowed to login with valid Usename and invalid Password*/
+	@Given("User will get a validation message when login with valid Usename and invalid Password")
+	public void circle_positive_tc_006() throws Exception {
+		try {
+			click("circles_mymedicare_username");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
+			click("circles_mymedicare_password");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
+			click("circles_mymedicare_login_button");
+			field_clear("circles_mymedicare_username");
+			field_clear("circles_mymedicare_username");
+			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
+			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
+		}catch (Exception e) {
+			e.printStackTrace();
+			takeScreenShot("circle_negative_tc_006");
+		}	
+	}
+
+	/*TC 007 - Validate that the user is not allowed to login with invalid Usename and valid Password */
+	@Given("User will get a validation message when login with invalid Usename and valid Password")
+	public void circle_positive_tc_007() throws Exception {
+		try {
+			click("circles_mymedicare_username");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_username"))).sendKeys(td_reader("circles_mymedicare_username",0));
+			click("circles_mymedicare_password");
+			driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_password"))).sendKeys(td_reader("circles_mymedicare_password",1));
+			click("circles_mymedicare_login_button");
+			field_clear("circles_mymedicare_username");
+			field_clear("circles_mymedicare_username");
+			str= driver.findElement(By.xpath(OR_reader("Object Locator","circles_mymedicare_error_msg"))).getText();
+			Assert.assertEquals(str,td_reader("circles_mymedicare_error_msg"));
+			close();
+		}catch (Exception e) {
+			e.printStackTrace();
+			takeScreenShot("circle_negative_tc_007");
+		}	
+	}
 }
